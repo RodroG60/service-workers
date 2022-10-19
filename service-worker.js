96a3a64f4b5d38ae39672e8524edc7cd0460f1a1
RodroG60 <105914008+RodroG60@users.noreply.github.com>
2022-10-19 18:14:05 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -13,11 +13,16 @@ self.addEventListener('statechange', function(e) {
   console.log('SW StateChange Event:', e);
 });
 
+const v1 = 'Inicial';
+
 self.addEventListener('fetch', function(event) {
   console.log('SW Fecth event');
   const url = new URL(event.request.url);
   console.log(url);
   console.log(url.pathname);
+  
+  console.log(v1);
+  v1 = 'Cambiado';
 
   //
   if (url.pathname === '/main/res.css') {
