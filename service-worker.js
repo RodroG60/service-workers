311ec782c9cb57a6acd8f1239d7183a2c3bc185c
RodroG60 <105914008+RodroG60@users.noreply.github.com>
2022-10-19 16:48:45 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,11 +5,23 @@ self.addEventListener('install', function(e) {
 });
 
 self.addEventListener('activate', function(e) {
+  clients.claim();
   console.log('SW Activate Event:', e);
 });
 
 self.addEventListener('statechange', function(e) {
   console.log('SW StateChange Event:', e);
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  console.log(url);
+
+  //
+  if (url.pathname === 'Prueba') {
+    console.log('Prueba');
+    event.respondWith('');
+  }
 });
 
 console.log('End SW');
